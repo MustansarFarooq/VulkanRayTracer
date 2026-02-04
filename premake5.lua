b1e84b9e244abce23f9ca829af3616ec66b9b117
Mustansar Farooq <97743639+MustansarFarooq@users.noreply.github.com>
2026-02-03 21:13:46 -0500

    Rename project from 'goofy ahh half functioning ray tracer' to 'rayTracer'

--- premake5.lua
+++ premake5.lua
@@ -2,9 +2,9 @@
 workspace "Raytracer"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "goofy ahh half functioning ray tracer"
+   startproject "rayTracer"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 include "Walnut/WalnutExternal.lua"
 
-include "goofy ahh half functioning ray tracer"
+include "rayTracer"
